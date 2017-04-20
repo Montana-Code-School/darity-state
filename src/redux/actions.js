bbf80d825f52f884bddb8e1092e5b3f0c103c27a
Nate Christianson <NateChristianson@Nates-MacBook-Air.local>
2017-04-20 14:39:23 -0600

    forgot to save

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,6 +1,6 @@
-const loginAction = (username, token) => ({type: 'Login', name: name, token: token})
+const loginAction = (name, token) => ({type: 'Login', name: name, token: token})
 
-const registerAction = (username, email, token) => ({type: 'Register', name: name, email: email, is_npo: is_npo, profilepic_path: profilepic_path, token: token})
+const registerAction = (name, email, is_npo, profilepic_path, token) => ({type: 'Register', name: name, email: email, is_npo: is_npo, profilepic_path: profilepic_path, token: token})
 
 const displayUserAction = (profile) => ({type: 'ViewProfile', profile: profile})
 
