ddeb63d6a80b82df5465a00af7f0905ffbcd6012
Nate Christianson <NateChristianson@Nates-MacBook-Air.local>
2017-04-25 15:03:18 -0600

    displaydare in redux

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import store from './redux/store'
 import User from './components/User'
 import loginReducer from './redux/reducer'
 import DisplayUser from './components/DisplayUser'
+import DisplayDare from './components/DisplayDare'
 module.exports = {
 	store: store,
 	User: User,
-	DisplayUser: DisplayUser
+	DisplayUser: DisplayUser,
+	DisplayDare: DisplayDare
 }
